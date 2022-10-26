27cf16ca59eb43d1892be90a455248cdb46d9a96
Jonathan Wang <jwang414@usc.edu>
2022-10-26 15:40:37 -0700

    update

--- j10w26g96HW8/homework8/src/app/app.module.ts
+++ j10w26g96HW8/homework8/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BusinessSearchComponent } from './business-search/business-search.component';
 
 
