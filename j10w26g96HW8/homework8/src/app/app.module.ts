8c1837a5d27c9db3e81ff97b5541a72a8c0127d7
Jonathan Wang <jwang414@usc.edu>
2022-10-26 12:09:58 -0700

    update homework8

---
+++ j10w26g96HW8/homework8/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BusinessSearchComponent } from './business-search/business-search.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BusinessSearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ j10w26g96HW8/homework8/src/app/business-search/business-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-search',
+  templateUrl: './business-search.component.html',
+  styleUrls: ['./business-search.component.css']
+})
+export class BusinessSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
